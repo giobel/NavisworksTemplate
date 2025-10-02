78e8580274a9847cfe9a00d51498f107b8be98ee
giobel <giobro@gmail.com>
2025-10-02 13:49:20 +0800

    Update LORaddin.cs

--- LORaddin.cs
+++ LORaddin.cs
@@ -1,17 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using swf = System.Windows.Forms;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+
+using Autodesk.Navisworks.Api;
 using Autodesk.Navisworks.Api.Plugins;
+using Autodesk.Navisworks.Api.Interop.ComApi;
+using ComApiBridge = Autodesk.Navisworks.Api.ComApi.ComApiBridge;
+
+
 namespace LOR_FBA
 {
     [Plugin("LORFBAAddin","FBA",DisplayName ="LOR-FBA")]
     [RibbonLayout("LORFBAAddin.xaml")]
     [RibbonTab("ID_CustomTab_1",DisplayName ="LOR-FBA")]
-    [Command("ID_Button_1", Icon = "1_16.ico", LargeIcon = "1_32.ico", ToolTip = "Test")]
-    [Command("ID_Button_2", Icon = "Two_16.ico", LargeIcon = "Two_32.ico", ToolTip = "Test")]
+    [Command("ID_Button_1", Icon = "add_16.png", LargeIcon = "add_32.png", ToolTip = "Add TimeLiner + Tab to selected items. This is necessary before copying properties from TimeLiner")]
+    [Command("ID_Button_2", Icon = "delete_16.png", LargeIcon = "delete_32.png", ToolTip = "Remove TimeLiner + Tab from selected items.")]
+    [Command("ID_Button_3", Icon = "3_16.png", LargeIcon = "3_32.png", ToolTip = "Copy TimeLiner properties in selected items.")]
+    [Command("ID_Button_4", Icon = "2_16.png", LargeIcon = "2_16.png", ToolTip = "Rename FBA properties")]
+
+
 
     public class LORaddin : CommandHandlerPlugin
     {
@@ -20,13 +28,19 @@ namespace LOR_FBA
             switch (name)
             {
                 case "ID_Button_1":
-                    MessageBox.Show("Button 1");
+                AddCategory.AddCategoryTab();
                     break;
                 case "ID_Button_2":
-                    MessageBox.Show("Button 2");
+                    DeleteCategory.DeleteCategoryTab();
+                    break;
+                case "ID_Button_3":
+                    CopyProperties.CopyTimeLinerProperties();
+                    break;
+                case "ID_Button_4":
+                    RenameProperties.RenamePropertiesFBA();
                     break;
             }
             return 0;
         }
-    }
+   }
 }
